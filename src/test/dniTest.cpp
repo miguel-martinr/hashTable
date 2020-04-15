cf281dc0266aa0ea5ca4cc946b7d27fdbeea8a7a
Alejandro <alu0101209777@ull.edu.es>
2020-04-15 18:00:55 +0100

    Testeo de algunas clases del proyecto

--- src/test/dniTest.cpp
+++ src/test/dniTest.cpp
@@ -1,3 +1,24 @@
+/**********************************************************************
+* Project           : Hash table implementation
+*
+* Class             : Testeo de la clase DNI
+*
+* Author            : Miguel Alejandro Martín Reyes (miguel-martinr)
+* e-mail            : alu0101209777@ull.edu.es
+* github            : https://github.com/miguel-martinr
+*
+* Date created      : 20200415
+*
+* References  :
+*    https://campusvirtual.ull.es/1920/pluginfile.php/209080/mod_resource/content/10/AEDA-pract04.pdf
+*
+*
+* Revision History  :
+*
+* Date        Author              Ref    Revision
+* 20200415    miguel-martinr      1      ...
+*
+**********************************************************************/
 #include "../../include/DNI.hpp"
 
 #include <iostream>
