11fb6554d83f584e3cd09e08c365b33a8d1470a1
Alejandro <alu0101209777@ull.edu.es>
2020-04-15 18:02:45 +0100

    Programa principal del experimento funcional

--- src/experimento/main.cpp
+++ src/experimento/main.cpp
@@ -1,3 +1,25 @@
+/**********************************************************************
+* Project           : Hash table implementation
+*
+* File              : Programa principal del experimento. Somete a prueba el
+*                     rendimiento de una tabla hash.
+*
+* Author            : Miguel Alejandro Martín Reyes (miguel-martinr)
+* e-mail            : alu0101209777@ull.edu.es
+* github            : https://github.com/miguel-martinr
+*
+* Date created      : 20200415
+*
+* References  :
+*    https://campusvirtual.ull.es/1920/pluginfile.php/209080/mod_resource/content/10/AEDA-pract04.pdf
+*
+*
+* Revision History  :
+*
+* Date        Author              Ref    Revision
+* 20200415    miguel-martinr      1      ...
+*
+**********************************************************************/
 #include "../../include/experimento/exp.hpp"
 
 #include <iostream>
